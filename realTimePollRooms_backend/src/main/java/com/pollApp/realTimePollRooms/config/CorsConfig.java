d3f2f66e368fda56bed59f5718c124d7b9d7f06b
YuvrajKhade <khadeyuvraj8@gmail.com>
2026-02-17 23:04:04 +0530

    added cors setting

--- realTimePollRooms_backend/src/main/java/com/pollApp/realTimePollRooms/config/CorsConfig.java
+++ realTimePollRooms_backend/src/main/java/com/pollApp/realTimePollRooms/config/CorsConfig.java
@@ -1,20 +1,27 @@
 package com.pollApp.realTimePollRooms.config;
 
 import org.apache.catalina.filters.CorsFilter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class CorsConfig {
 
+    @Value("${app.cors.allowed-origin}")
+    private String allowedOrigin;
     @Bean
     public CorsFilter corsFilter(){
+
+        List<String> origins= List.of(allowedOrigin.split(","));
+
         CorsConfiguration configuration=new CorsConfiguration();
         configuration.setAllowCredentials(true);
-        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedOriginPatterns(origins);
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
 
